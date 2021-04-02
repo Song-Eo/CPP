e03c064ce1960bb8413a00a5743cd2ac2b2c45af
Song-Eo <djwlsthd@naver.com>
2021-04-02 10:49:50 +0900

    prac

---
+++ base_practice/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    char name[10], number[15];
+    std::cout<<"What is your name?";
+    std::cin>>name;
+    std::cout<<"What is your phone number??";
+    std::cin>>number;
+    
+    std::cout<<"Name : "<<name<<"\n"<<"Phone_number : "<<number;
+
+}
